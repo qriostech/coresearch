d9ea264b4b57522f615e7c42aa5411911264fb6e
FilipAlexander <55953550+FilipAlexander@users.noreply.github.com>
2026-04-08 08:41:30 +0000

    removing rsync

--- rsync.sh
+++
@@ -1,2 +0,0 @@
-rsync -av /Users/filipuhlarik/coresearch -e "ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no" --exclude .lake_cache --exclude prepared_data chonker@192.168.101.15:/mnt/data
-    
